78d85ae081061ad0c6336ffc90c776c5e4858d33
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-12-25 22:32:08 +0500

    Initial Commit

---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by Thor on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
